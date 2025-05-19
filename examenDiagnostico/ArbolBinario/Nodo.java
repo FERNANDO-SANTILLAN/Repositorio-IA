7e53cb1b22bb2444b58ce1fe5f33c60d65b8d437
fersa <fersa@LAPTOP-JVHIVF6H>
2025-05-18 21:58:33 -0700

    examen diagnostico

---
+++ examenDiagnostico/ArbolBinario/Nodo.java
@@ -0,0 +1,13 @@
+package ArbolBinario;
+
+public class Nodo {
+    String nombre;
+    Nodo izq, der;
+    
+    public Nodo (String nombre){
+        this.nombre = nombre;
+        this.izq = null;
+        this.der = null;
+    }
+
+}
